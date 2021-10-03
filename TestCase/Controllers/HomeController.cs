b5d549ba13cad27bb248c9ca7af5839e4b0d6c72
Mert Savas <extmerts@d-teknoloji.com.tr>
2021-10-03 20:24:04 +0300

    first commit

---
+++ TestCase/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TestCase.IICUTech;
+
+namespace TestCase.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Home/Login")]
+        public string Login(string username, string password)
+        {
+            var client = new ICUTechClient();
+            var response = client.Login(username, password, string.Empty);         
+            return response;
+        }
+    }
+}
---
+++ TestCase/Scripts/main.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $("#loginForm").submit(function (e) {
+        e.preventDefault()
+        const username = $("input[name='username']").val();
+        const password = $("input[name='password']").val();
+        const data = {
+            username: username,
+            password: password
+        }
+
+        $.ajax({
+            url: '/Home/Login',
+            type: 'POST',
+            data: data,
+            dataType: 'json',
+            success: function (data) {
+                if (data.ResultCode == -1) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: data.ResultMessage,
+                    })
+                }
+                else {
+                    Swal.fire({
+                        icon: 'Success',
+                        title: 'Welcome ' + data.FirstName + " " + data.LastName,
+                        html: "<pre> Your Phone: " + data.Mobile+ "\n" + "Your Email :" + data.Email + "</pre>",
+                    })
+                }
+                
+            },
+            error: function (request, error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Technical Error',
+                })
+            }
+        });
+    })
+});
+
